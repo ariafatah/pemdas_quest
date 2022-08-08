e0c3a50d14a33205f6f88f0ced8cc2a914abfb00
ariafatah <110509117+ariafatah@users.noreply.github.com>
2022-08-07 18:51:15 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+#belajar output
+
+print("hello world")
+print("nama saya aria fatah")
+print("teman teman saya memanggil saya aria")
+print("saya anak pertama dari dua bersauda")
+print("saya berumur 15 tahun")
+print("saya tinggal di indonesia")
+print("saya mempunyai hobby bermain game")
+print("rumah saya di depok cimanggis kelapa dua")
+print("saya sekolah di SMK HARPAN BANGSA")
+print("saya penyuka anime")
---
+++ python2.py
@@ -0,0 +1,14 @@
+#belajar input
+
+nama = "aria" # aria adalah data, nama adalah variabel
+umur = 15
+alamat = "cimanggis, kota depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print("nama saya adalah " + nama + " umur saya " + str (umur) + " alamat saya " + alamat)
+
+print("nama saya adalah " + nama)
+
