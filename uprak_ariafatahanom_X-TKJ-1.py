07c2fa045a2cb56a805d278d1852c0efef6542c9
ariafatah <110509117+ariafatah@users.noreply.github.com>
2022-08-12 10:31:38 +0700

    Add files via upload

---
+++ uprak_ariafatahanom_X-TKJ-1.py
@@ -0,0 +1,11 @@
+#tugas pratikum
+#SOAL NO 1
+nama_depan = input("masukan nama depan: ")
+nama_belakang = input("masukan nama belakang: ")
+
+print("nama anda adalah", nama_depan, nama_belakang)
+
+#soal no 2
+celcius = int(input("masukan suhu celcius: "))
+reamur = (4/5 * celcius)
+print(celcius, "celcius adalah ", reamur, "reamur")
